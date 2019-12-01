4de28b589e5a33a33765fe72ced124245ad178b0
Jaety Edwards <jaetye@gmail.com>
2019-11-30 22:09:47 -0500

    adding people data and markers

--- jaesite/src/App.js
+++ jaesite/src/App.js
@@ -10,6 +10,13 @@ import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
+import Drawer from '@material-ui/core/Drawer';
+
+// import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
+// import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+// import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+
 
 import Button from '@material-ui/core/Button';
 
@@ -25,16 +32,70 @@ TabContainer.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
+const drawerWidth = 240;
+
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
   },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
 }));
+
 
 function App() {
   const classes = useStyles();
   const [value, setValue] = React.useState(1);
+  const [open, setOpen] = React.useState(false);
 
   function handleChange(event, newValue) {
     setValue(newValue);
@@ -61,6 +122,23 @@ function App() {
     </div>
   );
 }
+
+/*
+<Drawer
+   className={classes.drawer}
+   variant="persistent"
+   anchor="right"
+   open={true}
+   classes={{
+     paper: classes.drawerPaper,
+   }}
+ >
+  <div className={classes.drawerHeader}>
+  </div>
+  Hello I'm a drawer
+ </Drawer>
+
+*/
 
 /*
 <div className="App">
--- jaesite/src/HistoryMap.js
+++ jaesite/src/HistoryMap.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { Map, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet';
+import L from 'leaflet';
+import { Map, TileLayer, Marker, Popup, Rectangle, Tooltip, GeoJSON } from 'react-leaflet';
+import { MapControl, PropTypes } from 'react-leaflet';
+// import DivIcon from 'react-leaflet-div-icon';
 import 'leaflet/dist/leaflet.css';
 
+delete L.Icon.Default.prototype._getIconUrl;
+
+
+// https://github.com/PaulLeCam/react-leaflet/issues/453
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 function getColor(input, scale) {
     var colors = ['#FFEDA0','#FED976','#FEB24C','#FD8D3C','#FC4E2A','#E31A1C','#BD0026','#800026'];
-    return colors[Math.floor( (input / scale) * (colors.length-1) )]
+    return colors[Math.min(Math.floor( (input / scale) * (colors.length-1) ), colors.length-1)];
     // let d = input / 100000;
     // return d > 1000 ? '#800026' :
     //        d > 500  ? '#BD0026' :
@@ -19,7 +31,7 @@ function getColor(input, scale) {
 
 function style(feature) {
     return {
-        fillColor: getColor(feature.properties.population, 100),
+        fillColor: getColor(feature.properties.population, 1000),
         weight: 2,
         opacity: 1,
         color: 'white',
@@ -43,6 +55,9 @@ function highlightFeature(e) {
     // }
 }
 
+function point2position(point) {
+  return [point.coordinates[1], point.coordinates[0]];
+}
 
 
 class HistoryMap extends React.Component {
@@ -52,7 +67,10 @@ class HistoryMap extends React.Component {
       lat: -19.0154,
       lng: 29.1549,
       zoom: 3,
-      datasets: []
+      datasets: [],
+      people: {rows:[]},
+      highlightCenter: undefined,
+      hightlightCountry: undefined,
     }
   }
 
@@ -60,10 +78,28 @@ class HistoryMap extends React.Component {
     fetch('http://localhost:5000')
       .then(response => response.json())
       .then(dataset => this.setState({datasets: [dataset]}))
+    // fetch('http://localhost:5000/people')
+    //   .then(response => response.json())
+    //   .then(dataset => {
+    //     this.setState({people: dataset});
+    //   })
+  }
+
+  setHighlight(e) {
+    highlightFeature(e);
+    const center = e.target.getBounds().getCenter();
+    this.setState({
+      highlightCenter: [center.lat, center.lng],
+      highlightCountry: e.target.feature
+    });
   }
 
   resetHighlight(e) {
       this.refs.boundariesLayer.leafletElement.resetStyle(e.target);
+      this.setState({
+        highlightCenter: undefined,
+        highlightCountry: undefined
+      });
   }
 
   zoomToFeature(e) {
@@ -72,22 +108,74 @@ class HistoryMap extends React.Component {
 
   onEachFeature(feature, layer) {
       layer.on({
-          mouseover: highlightFeature,
+          mouseover: this.setHighlight.bind(this),
           mouseout: this.resetHighlight.bind(this),
           click: this.zoomToFeature.bind(this)
       });
   }
 
+  handleMoveEnd(e) {
+    function buildUrl(base, bounds) {
+      return `http://localhost:5000/${base}?minx=${bounds.getWest()}&maxx=${bounds.getEast()}&miny=${bounds.getSouth()}&maxy=${bounds.getNorth()}`
+    }
+
+    const bounds = e.target.getBounds();
+    console.log(bounds);
+    console.log(buildUrl("count_in_box",bounds))
+    fetch(buildUrl("count_in_box",bounds))
+      .then(response => response.json())
+      .then(count => {
+        console.log("count",count, count<75)
+        if (count < 200) {
+          fetch(buildUrl("people_in_box",bounds))
+            .then(response => response.json())
+            .then(dataset => {
+              console.log(dataset)
+              this.setState({people: dataset});
+            })
+        } else {
+          this.setState({
+            people: {rows:[]}
+          })
+        }
+      });
+  }
+
   render() {
     const position = [this.state.lat, this.state.lng];
+    const highlightPosition = this.state.highlightCenter;
+
     const mapboxAccessToken = 'pk.eyJ1IjoiamFldHkiLCJhIjoiY2p5Y3hpaDNtMGF6MTNwanprY2lmZGtoaSJ9.vv8A-gUvtzjeAkU8oNGHQw';
     const mapUrl = 'https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken;
     const mapAttribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
 			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
 
+
+    // const rectBounds = (!highlightPosition) ? null : (
+    //   [[highlightPosition[0]-20, highlightPosition[1]-20],
+    //    [highlightPosition[0]-10, highlightPosition[1]-10]]
+    //  );
+    var marker = null;
+    if (highlightPosition) {
+      const rectBounds = [highlightPosition, highlightPosition];
+      const count = this.state.highlightCountry.properties.population;
+      marker = (!highlightPosition) ? null : (
+        <Rectangle bounds={rectBounds}>
+          <Tooltip permanent>{count}</Tooltip>
+        </Rectangle>
+        // <Marker position={highlightPosition}>
+        //   <Popup>
+        //     A pretty CSS3 popup. <br/> Easily customizable.
+        //   </Popup>
+        // </Marker>
+      )
+    }
+    const rows = this.state.people.rows;
+    console.log(rows)
+    console.log(rows.map((row,idx) => point2position(row[2])))
     return (
-      <Map center={position} zoom={this.state.zoom} ref="map">
+      <Map center={position} zoom={this.state.zoom} ref="map" onMoveEnd={this.handleMoveEnd.bind(this)}>
         <TileLayer
           attribution={mapAttribution}
           url={mapUrl}
@@ -101,14 +189,20 @@ class HistoryMap extends React.Component {
             ref="boundariesLayer"
           />)
         })}
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br/> Easily customizable.
-          </Popup>
-        </Marker>
+        { marker }
+        {this.state.people.rows.map((row,idx) =>
+          <Marker key={`marker-${idx}`} position={point2position(row[2])} autoPan="false">
+            <Popup>
+              <p><span><a href={row[0]}>{row[1]}</a></span></p>
+              <p>{row[3]}</p>
+            </Popup>
+          </Marker>
+        )}
       </Map>
     );
   }
 }
+
+
 
 export default HistoryMap;
