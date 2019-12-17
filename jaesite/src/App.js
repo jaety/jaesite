b3bf9030950f112f300b0f28161edad6016b51fd
Jaety Edwards <jaetye@gmail.com>
2019-12-16 19:32:48 -0500

    cleanup of App.js

--- jaesite/src/App.js
+++ jaesite/src/App.js
@@ -10,19 +10,10 @@ import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
-// import Drawer from '@material-ui/core/Drawer';
-// import Button from '@material-ui/core/Button';
-
-// import IconButton from '@material-ui/core/IconButton';
-// import MenuIcon from '@material-ui/icons/Menu';
-// import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-// import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-
-
 
 function TabContainer(props) {
   return (
-    <Typography component="div" style={{ padding: 8 * 3 }}>
+    <Typography component="div" style={{ padding: 6 * 3 }}>
       {props.children}
     </Typography>
   );
@@ -32,133 +23,38 @@ TabContainer.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
-const drawerWidth = 240;
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-  },
-  appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  hide: {
-    display: 'none',
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: -drawerWidth,
-  },
-  contentShift: {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  },
-}));
-
-
 function App() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(2);
-  // const [open, setOpen] = React.useState(false);
-
-  function handleChange(event, newValue) {
-    setValue(newValue);
-  }
+  const [tabIndex, setTabIndex] = React.useState(2);
 
   return (
-    <div className={classes.root}>
+    <div>
       <AppBar position="static">
-        <Tabs value={value} onChange={handleChange}>
+        <Tabs value={tabIndex} onChange={ (event, newValue) => setTabIndex(newValue) }>
           <Tab label="Intro" />
           <Tab label="Map" />
           <Tab label="Table" />
         </Tabs>
       </AppBar>
-      {value === 0 && <TabContainer>This is the Introduction</TabContainer>}
-      {value === 1 && <TabContainer>
+
+      {tabIndex === 0 &&
+        <TabContainer>This is the Introduction</TabContainer>
+      }
+
+      {tabIndex === 1 &&
+        <TabContainer>
           <div className="leaflet-container">
             <HistoryMap></HistoryMap>
           </div>
-        </TabContainer>}
-      {value === 2 && <TabContainer>
+        </TabContainer>
+      }
+
+      {tabIndex === 2 &&
+        <TabContainer>
           <HistoryTable></HistoryTable>
-        </TabContainer>}
+        </TabContainer>
+      }
     </div>
   );
 }
 
-/*
-<Drawer
-   className={classes.drawer}
-   variant="persistent"
-   anchor="right"
-   open={true}
-   classes={{
-     paper: classes.drawerPaper,
-   }}
- >
-  <div className={classes.drawerHeader}>
-  </div>
-  Hello I'm a drawer
- </Drawer>
-
-*/
-
-/*
-<div className="App">
-  <div class="leaflet-container">
-    <HistoryMap></HistoryMap>
-  </div>
-  <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-      Edit <code>src/App.js</code> and save to reload.
-    </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
-  </header>
-</div>
-*/
 export default App;
